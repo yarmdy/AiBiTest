66f57b7d20c0ef00fcfb73e7f8b62420b27c23be
yarmdy <jxy2000121@hotmal.com>
2023-09-19 17:38:04 +0800

    公司法

--- AiBi.Web/Global.asax.cs
+++ AiBi.Web/Global.asax.cs
@@ -30,21 +30,74 @@ namespace AiBi.Test.Web
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
 
-        protected void Application_Error(object sender, EventArgs e) { 
+        protected void Application_Error(object sender, EventArgs e)
+        {
             var ex = Server.GetLastError();
-            var code = ex is HttpException ? (ex as HttpException).GetHttpCode(): 500;
-            var title = code!=0 ? code+"" : "发生服务器错误";
+            var code = ex is HttpException ? (ex as HttpException).GetHttpCode() : 500;
+            var title = code != 0 ? code + "" : "发生服务器错误";
             var message = ex.GetInnerMessage();
 
             Server.ClearError();
 
             var controller = new HomeController();
-            controller.ControllerContext = new ControllerContext(Request.RequestContext,controller);
-            var result = new ViewResult { ViewName="Error"};
-            result.ViewBag.ErrorTitle = title;
-            result.ViewBag.ErrorMessage = message;
+            controller.ControllerContext = new ControllerContext(Request.RequestContext, controller);
+            ActionResult result;
+            if (Request.IsAjaxRequest())
+            {
+                var result2 = new JsonResult();
+                var res = new Response();
+                res.code = EnumResStatus.Fail;
+                res.msg = message;
+                result2.Data = res;
+                result2.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                result = result2;
+            }
+            else
+            {
+                var result2 = new ViewResult { ViewName = "Error" };
+                result2.ViewBag.ErrorTitle = title;
+                result2.ViewBag.ErrorMessage = message;
+                result = result2;
+            }
             Response.StatusCode = code;
             result.ExecuteResult(controller.ControllerContext);
+        }
+    }
+
+    public static class AjaxRequestExtensionsHttpRequest
+    {
+        //
+        // 摘要:
+        //     Determines whether the specified HTTP request is an AJAX request.
+        //
+        // 参数:
+        //   request:
+        //     The HTTP request.
+        //
+        // 返回结果:
+        //     true if the specified HTTP request is an AJAX request; otherwise, false.
+        //
+        // 异常:
+        //   T:System.ArgumentNullException:
+        //     The request parameter is null (Nothing in Visual Basic).
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            if (!(request["X-Requested-With"] == "XMLHttpRequest"))
+            {
+                if (request.Headers != null)
+                {
+                    return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+                }
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
